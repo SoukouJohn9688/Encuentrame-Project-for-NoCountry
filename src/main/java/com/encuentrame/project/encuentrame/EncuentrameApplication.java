2bfe47f0b8ad800ef30b6039cc23589f4c0131d3
MatiCampo96 <maticampopiano@hotmail.com>
2024-02-16 20:44:38 -0300

    Primer commit

---
+++ src/main/java/com/encuentrame/project/encuentrame/EncuentrameApplication.java
@@ -0,0 +1,13 @@
+package com.encuentrame.project.encuentrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuentrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuentrameApplication.class, args);
+	}
+
+}
