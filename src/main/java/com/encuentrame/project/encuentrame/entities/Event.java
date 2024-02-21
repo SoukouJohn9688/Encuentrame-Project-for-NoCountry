69152ce48eb2d6b73af4529713f3710dff5bf3ee
MatiCampo96 <maticampopiano@hotmail.com>
2024-02-21 11:11:11 -0300

    adding entities Event & User

--- src/main/java/com/encuentrame/project/encuentrame/entities/Event.java
+++ src/main/java/com/encuentrame/project/encuentrame/entities/Event.java
@@ -1,5 +1,35 @@
 package com.encuentrame.project.encuentrame.entities;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "events")
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDTENITY)
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime date_time;
+    private String city;
+    private String address;
+    @ManyToOne
+    @JoinColumn(name = "id_admin", nullable = false)
+    private User user;
     
 }
--- src/main/java/com/encuentrame/project/encuentrame/entities/User.java
+++ src/main/java/com/encuentrame/project/encuentrame/entities/User.java
@@ -1,13 +1,34 @@
 package com.encuentrame.project.encuentrame.entities;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
-    int id;
-    String first_name;
-    String last_name;
-    String email;
-    String password;
-    
-
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDTENITY)
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private LocalDate birthdate;
+    private enum role{
+        USER,
+        MODERATOR,
+        ADMIN
+    }
 }
