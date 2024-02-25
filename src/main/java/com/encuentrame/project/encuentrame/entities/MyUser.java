25c10deec0d78231bec0e1fc1d9b0e309c12d901
JuanCJ9688 <ironjohn788@gmail.com>
2024-02-25 14:32:29 -0500

    Added Spring Security configuration implementation. Refactored User Entity to MyUser to avoid conflicts with Spring Security User reserved word. Implemented MyUserService.

--- src/main/java/com/encuentrame/project/encuentrame/entities/MyUser.java
+++ src/main/java/com/encuentrame/project/encuentrame/entities/MyUser.java
@@ -2,9 +2,7 @@ package com.encuentrame.project.encuentrame.entities;
 
 import com.encuentrame.project.encuentrame.enumerations.UserRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -13,10 +11,13 @@ import java.util.Set;
 import java.util.UUID;
 
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class User {
+
+public class MyUser {
     @Id
     @UuidGenerator
     @Column(name = "user_id")
@@ -40,4 +41,6 @@ public class User {
     private LocalDate birthdate;
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+
 }
